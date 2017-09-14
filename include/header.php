6295e277aaf4f6673a92a2689c31d91f916b9280
CathyThuliez <cathy.thuliez@crea-tech.fr>
2017-09-14 14:47:46 +0200

    Add css folde and file

--- include/header.php
+++ include/header.php
@@ -13,6 +13,8 @@ $connection = new mysqli(
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="vendor/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
   <script type="text/javascript" src="vendor/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
   <title>TP PHP: Gestion des promotions et des élèves</title>
 </head>
@@ -24,6 +26,8 @@ $connection = new mysqli(
           <li><a href="/">Accueil</a></li>
           <li><a href="promotions.php">Promotions</a></li>
           <li><a href="students.php">Elèves</a></li>
+          <li><a href="create_promotion.php">Ajouter une promotion</a></li>
+          <li><a href="create_student.php">Ajouter un élève</a></li>
         </ul>
       </div>
     </div>
--- promotions.php
+++ promotions.php
@@ -11,7 +11,7 @@
             while ($row = $result->fetch_assoc()) {
               printf('
                 <div class="form-group">
-                  <label class="col-md-4 control-label" for="edit_button">%s n°%s</label>
+                  <label class="col-md-4 control-label" for="edit_button">n°%s %s</label>
                   <div class="col-md-8">
                     <a href="change_promotion.php?id=%s" id="edit_button%s" name="edit%s" class="btn btn-success">Éditer</a>
                     <a href="delete_promotion.php?id=%s" id="del_button%s" name="del%s" class="btn btn-danger">Supprimer</a>
